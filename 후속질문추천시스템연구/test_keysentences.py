1f8febdd8968f528c59f5382c9de8c0225f11650
JiunChoi <chlwldns00@naver.com>
2023-11-29 10:41:55 +0900

    1129

--- 후속질문추천시스템연구/test_keysentences.py
+++ 후속질문추천시스템연구/test_keysentences.py
@@ -9,26 +9,33 @@ import pandas as pd
 
 
 ## answer 전처리 ##
-df=pd.read_csv("textranks/textrank-master/JEUS_application-client_final_DB(문단)_0705_new_eng.csv",encoding='utf-8',header=None)
+df=pd.read_csv("후속질문추천시스템연구/JEUS_application-client_final_DB(문단)_0705_new_eng.csv",encoding='utf-8',header=None)
 df=df[3]
-df=df[:10]  
+#df=df[:10]  
 df_list=df.values.tolist()
 f=[]
 for txt in df_list:
     txt=txt.replace('\n','')
-    #txt=txt.replace('.','')
     txt=txt.replace(',','')
     txt=txt.replace('"','')
     txt=txt.replace("'",'')
     txt=txt.replace('-','')
+    txt=txt.replace('(','')
+    txt=txt.replace(')','')
+    txt=txt.replace('{','')
+    txt=txt.replace('}','')
+    txt=txt.replace('`','')
+    txt=txt.replace('<','')
+    txt=txt.replace(':','')
+    txt=txt.replace('>','')
                     
     f.append(txt)
 #f='\n'.join(f)
-sent=f[0]
-#print(sent)
-sent=sent.split('.')
-print(sent)
-print(type(sent))
+# sent=f[0]
+# #print(sent)
+# sent=sent.split('.')
+# print(sent)
+# print(type(sent))
 print('\n------------------------------------------')
 #### 형태소 분석기 함수화 ####
 t=Okt()
@@ -53,6 +60,28 @@ summarizer = KeysentenceSummarizer(
     verbose = False
 )
 #print(sent)
-keysents = summarizer.summarize(sent, topk=1)
-print(type(keysents))
-print(keysents)
+answerKeyword_list=[]
+## 반복문으로 전구간 순회 
+
+for i in range(0,678):
+
+    sent=f[i]
+    sent=sent.split('.')
+    #print(sent)
+
+    keysents = summarizer.summarize(sent, topk=3) 
+    # print(type(keysents))
+    # print(keysents)
+    print(i)
+    for j in range(len(keysents)):
+        s=keysents[j][2]+'\n'
+
+    answerKeyword_list.append(s)
+    s=''
+
+print(answerKeyword_list[-1])
+
+
+# keysents = summarizer.summarize(sent, topk=1)
+# print(type(keysents))
+# print(keysents)
--- 후속질문추천시스템연구/답변DB의답변들에서 핵심문장들을 뽑아 합친 문단DB작성.py.py
+++ 후속질문추천시스템연구/답변DB의답변들에서 핵심문장들을 뽑아 합친 문단DB작성.py.py
@@ -3,15 +3,16 @@ import pandas as pd
 from konlpy.tag import Okt
 import os 
 import openai
+import csv
 openai.api_key = "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"
 
 t=Okt()
 df=pd.read_csv("후속질문추천시스템연구/JEUS_application-client_final_DB(문단)_0705_new_eng.csv",encoding='utf-8',header=None)
-df=df[3]
+df=df[3]  ##답변데이터들
 df_list=df.values.tolist()
 tokenized=[]
 
-def tokenize(df):
+def pre_processing(df):
     for sentences in df:  #### 전처리및 토크나이징
         
         sentences=str(sentences)
@@ -22,42 +23,38 @@ def tokenize(df):
         sentences=sentences.replace('-','')
         sentences=sentences.replace('(','')
         sentences=sentences.replace(')','')
-        sentences=sentences.replace('.','')
+        #sentences=sentences.replace('.','')
+        sentences=sentences.replace('{','')
+        sentences=sentences.replace('}','')
+        sentences=sentences.replace('`','')
+        sentences=sentences.replace('<','')
+        sentences=sentences.replace(':','')
+        sentences=sentences.replace('>','')
         #print(sentences)
-        contents_tokens=t.morphs(sentences)
-        tokenized.append(contents_tokens)
+        tokenized.append(sentences)
     
     return tokenized
 
+# def token_to_fullsent(contents_tokens):
+#     ttok_sentence_forVectorize=[]
 
+#     for content in contents_tokens:
+#         sentence=''
+#         for word in content:
+#             sentence=sentence+' '+word
+        
 
-def keyword_extractor_Qanswer(tokenizinedList):
-    idx=0 #### gpt로 키워드 뽑는 부분
+#         ttok_sentence_forVectorize.append(sentence)
+#     return ttok_sentence_forVectorize
 
-    answer=str(tokenizinedList[idx]) #토큰화된것중에 타겟질문에 대한 해답을 index로 반환해주는 idx
-    context='' #gpt 시스템에서는 context가 존재하지않는거 같다. 혹은 context로 어떤것을 넣어줘야할지는 미정.
-    prompt=context+answer+' 다음 문장의 핵심 키워드들을 문맥을 참고하여 중요한 순서대로 dict자료형으로 뽑아줘'
-    completion = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role":"user","content":prompt}
-        ]
-    )
+t=pre_processing(df)
 
-
-    keyword_dict=completion.choices[0].message.content
-    # print(type(keyword_dict))
-    keyword_dict=eval(keyword_dict) #str->dict type difference
-
-    print(completion.choices[0].message.content)
-    return keyword_dict
 
 
 def okt_tokenizer(sents):
 
     #### 형태소 단위로 tokenize
-    contents_tokens=t.morphs(sents)
+    contents_tokens=[t.morphs(sent) for sent in sents]
     return contents_tokens #->list
 
 
@@ -69,16 +66,39 @@ def okt_tokenizer(sents):
 ##### 여기서 부터 작업1128
 
 summarizer = KeysentenceSummarizer(
-    tokenize = tokenize,
+    tokenize = okt_tokenizer,
     min_sim = 0.3,
     verbose = False
 )
+
+answerKeyword_list=[]
 ## 반복문으로 전구간 순회 
-keysents = summarizer.summarize(sent, topk=3)
+
+    
+sent=t[0]
+sent=sent.split('.')
+print(sent)
+
+keysents = summarizer.summarize(sent, topk=3) 
 print(type(keysents))
 print(keysents)
+for j in range(0,3):
+    s=keysents[j][2]+'\n'
+answerKeyword_list.append(s)
+s=''
 
-### keysents 를 합치는 작업(하나의 문단으로 만듬)
+# answerKeyword_list.insert('답변핵심문장모음문단',0)
+# ## s를 csv에 추가/합침
+
+# csv_filename = "답변핵심문장모음문단.csv"
+
+# with open(csv_filename, mode="w", newline="", encoding="utf-8") as file:
+#     writer = csv.writer(file)
+    
+#     # 리스트 데이터를 CSV 파일에 쓰기
+#     writer.writerows(answerKeyword_list)
+
+# ### keysents 를 합치는 작업(하나의 문단으로 만듬)
 
 
 
