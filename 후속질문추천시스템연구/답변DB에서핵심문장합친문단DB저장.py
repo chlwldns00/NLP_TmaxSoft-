4772877e706d4e2f33481d994d6cce918b5845f4
JiunChoi <chlwldns00@naver.com>
2023-12-04 11:24:03 +0900

    1124

--- 후속질문추천시스템연구/답변DB에서핵심문장합친문단DB저장.py
+++ 후속질문추천시스템연구/답변DB에서핵심문장합친문단DB저장.py
@@ -1,19 +1,15 @@
-from textrank import KeywordSummarizer
-import konlpy
+from textrank import KeysentenceSummarizer
 from konlpy.tag import Okt
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-import scipy as sp
-
-import pandas as pd
+import csv
 
 
-## answer 전처리 ##
-df=pd.read_csv("후속질문추천시스템연구/JEUS_application-client_final_DB(문단)_0705_new_eng.csv",encoding='utf-8',header=None)
+
+## answer 전처리(데이터셋읽어오기) ##
+df=pd.read_csv("후속질문추천시스템연구/JEUS_application-client_final_DB(문단)_0705_new_eng copy.csv",encoding='utf-8',header=None)
 df=df[3]
-#df=df[:10]  
 df_list=df.values.tolist()
-f=[]
+df_answer_list=[]
 for txt in df_list:
     txt=txt.replace('\n','')
     txt=txt.replace(',','')
@@ -29,46 +25,39 @@ for txt in df_list:
     txt=txt.replace(':','')
     txt=txt.replace('>','')
                     
-    f.append(txt)
-#f='\n'.join(f)
-# sent=f[0]
-# #print(sent)
-# sent=sent.split('.')
-# print(sent)
-# print(type(sent))
-print('\n------------------------------------------')
+    df_answer_list.append(txt)
+
+print('전처리 완료 \n------------------------------------------')
+
 #### 형태소 분석기 함수화 ####
 t=Okt()
 
 
 def okt_tokenizer(sents):
 
-    #### 형태소 단위로 tokenize
     contents_tokens=t.morphs(sents)
     return contents_tokens #->list
 
 
-
-# a=okt_tokenizer(sent)
-# print(a)
 # textrank사용해서 키워드 추출 ##
-from textrank import KeysentenceSummarizer
+
 
 summarizer = KeysentenceSummarizer(
     tokenize = okt_tokenizer,
     min_sim = 0.3,
     verbose = False
 )
-#print(sent)
-answerKeyword_list=[]
-## 반복문으로 전구간 순회 
-s=''
-for i in range(0,678):
 
-    sent=f[i]
+
+## 반복문으로 전구간 순회하여 답변DB의 핵심문장모음문단 만들기(keyword뽑기용)
+answerKeyword_list=[]
+s=''
+for i in range(len(df_answer_list)):
+
+    sent=df_answer_list[i]
     sent=sent.split('.')
     #print(sent)
-    if len(sent)<=4:                   #405번째 column에서 에러 여기 부터 해결 11/29 => 에러내용확인
+    if len(sent)<=4:                 #error수정사항#  
                                     #에러내용=>summarizer함수에서 리스트내 문장중 topk만큼의 핵심문장을 비교 / 선택 하는데 답변이 3문장이 안되는 답변리스트 요소가 exception코드에 걸린거같다.
         for k in range(len(sent)): #답변이 너무짧아 핵심문장을 못뽑는경우는 그냥 전부를 문장단위로 자른뒤 토크나이징및 전처리만 함
             s=sent[k]+'\n'
@@ -89,7 +78,29 @@ for i in range(0,678):
 
 print(answerKeyword_list[0])
 
+## 기존 [메뉴얼,질문,답변] DB에 답변의 핵심문장문단 추가 코드 ##
 
-# keysents = summarizer.summarize(sent, topk=1)
-# print(type(keysents))
-# print(keysents)
+# 기존 CSV 파일 경로
+csv_filename = "후속질문추천시스템연구/JEUS_application-client_final_DB(문단)_0705_new_eng copy.csv"
+
+# CSV 파일 열기 (기존 데이터를 읽어오기 위해)
+with open(csv_filename, mode="r", newline="", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    
+    # 기존 데이터 읽어오기
+    existing_data = list(reader)
+
+# 새로운 데이터를 열로 추가
+for i, item in enumerate(answerKeyword_list):
+    # 각 행에 해당 열의 데이터 추가
+    existing_data[i].append(item)
+
+# CSV 파일 열기 (추가된 데이터를 쓰기 위해)
+with open(csv_filename, mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    
+    # 추가된 데이터를 CSV 파일에 쓰기
+    writer.writerows(existing_data)
+
+print(f"{csv_filename} 파일에 새로운 행이 추가되었습니다.")
+
